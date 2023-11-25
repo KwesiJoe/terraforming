db4cd3cea8853c47c71a2d22e89985139ef14e76
Joseph Sarpong <joseph-sarpong@hotmail.com>
2023-11-25 12:07:16 +0000

    read aws region from tfvars

--- AWS/components/s3 and cloudfront/terraform.tfvars
+++ AWS/components/s3 and cloudfront/terraform.tfvars
@@ -1,1 +1,2 @@
-# buckets = ["bucket-1", "bucket-2"]
+buckets = ["bucket-1", "bucket-2"]
+region = "your-aws-region"
--- AWS/components/s3 and cloudfront/variables.tf
+++ AWS/components/s3 and cloudfront/variables.tf
@@ -2,3 +2,8 @@ variable "buckets" {
   type = list(string)
   description = "names of buckets to create"
 }
+
+variable "region" {
+  type = string
+  description = "aws region"
+}
